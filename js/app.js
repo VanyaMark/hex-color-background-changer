b4b038c1c5208fc6836bb427ba4e33e8672722fd
VanyaMark <markova.vanya@gmail.com>
2024-05-02 21:13:09 +0200

    done with hex color and added in title

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 // Implementar solución aquí
 const body = document.querySelector('body');
 const btn = document.querySelector('#btn');
+const title = document.querySelector('#hex-value')
 
 const randomColor = () => Math.floor(Math.random() * 256);
 
+function generateHexColor() {
+    const makingColorCode = '0123456789ABCDEF';
+    let finalCode = '#';
+    for (let counter = 0; counter < 6; counter++) {
+        finalCode += makingColorCode[Math.floor(Math.random() * 16)];
+    }
+    return finalCode;
+}
+
 const changeColor = () =>{
-    body.style.backgroundColor = (`rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`)
-    console.log('change')
+    let color = generateHexColor();
+    body.style.backgroundColor = color;
+    title.textContent = color;
+
 }
 
 btn.onclick = changeColor;
