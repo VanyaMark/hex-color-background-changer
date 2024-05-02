02471b25496a63c23621d55cf73026f3a46a7aa7
VanyaMark <markova.vanya@gmail.com>
2024-05-02 20:27:05 +0200

    exercise done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 // Implementar solución aquí
+const body = document.querySelector('body');
+const btn = document.querySelector('#btn');
 
+const randomColor = () => Math.floor(Math.random() * 257);
+
+const changeColor = () =>{
+    body.style.backgroundColor = (`rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`)
+    console.log('change')
+}
+
+btn.onclick = changeColor;
